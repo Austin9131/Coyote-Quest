362787507f3ded88e42b137713f45044a5cc4ec4
DragonSlayer783 <JacobDiba654@gmail.com>
2021-10-19 20:48:30 -0700

    Added Styles sheet

---
+++ styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import {View} from 'react-native';
+import Constants from 'expo-constants';
+
+
+
+//colors
+export const Colors = {
+
+primary: '#ffffff',
+secondary: '#E5SE7EB',
+tertiary: '#1F2937',
+darklight: '#9CA3AF',
+brand: '#6D28D9',
+green: '#10B981',
+red: '#EF4444',
+};
+
+const{ primary, secondary, tertiary, darklight, brand, green,red } = Colors;
+
