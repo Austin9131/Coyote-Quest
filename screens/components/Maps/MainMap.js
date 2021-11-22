ca539105ace603d7f1ebb00b55d44bbd3ef2ddfb
kadockham <kdockham2018@gmail.com>
2021-11-21 19:43:42 -0800

    Made some markers

--- screens/components/Maps/MainMap.js
+++ screens/components/Maps/MainMap.js
@@ -1,8 +1,16 @@
 import MapView from 'react-native-maps';
+import Marker from 'react-native-maps';
 import * as React from 'react';
 import { maps } from '../../../styles';
 
-export const MainMap = () => {
+export const MainMap = ({ input }) => {
+  state = {
+    markers:[
+      { id: 'UH', name: 'University Hall', latitude: 34.1804275762316, longitude: -117.3242646204980 },
+      { id: 'SUE', name: 'Student Union East', latitude: 34.1804453275725, longitude: -117.3231863724740 },
+      { id: 'SUS', name: 'Student Union South', latitude: 34.1813062631255, longitude: -117.3234599577940 },
+    ]
+  }
     return(
         <MapView 
             ref={(ref) => this.mapRef = ref }
@@ -19,6 +27,15 @@ export const MainMap = () => {
               { latitude: 34.188731, longitude: -117.330944 },
               { latitude: 34.176092, longitude: -117.313079 })}}
             minZoomLevel = {14.5}
-          />
+          >
+            
+            {this.state.markers.map(( marker, index ) => (
+              <MapView.Marker
+                key={index}
+                title={marker.name}
+                coordinate={{ latitude: marker.latitude ,longitude: marker.longitude }}
+              />
+            ))}
+          </MapView>
     );
 };
