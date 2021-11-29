9095f40e69796695616f065fa87815c5cb049ba0
DragonSlayer783 <JacobDiba654@gmail.com>
2021-11-28 23:49:15 -0800

    added to polyline array

--- screens/components/Maps/AuthMap.js
+++ screens/components/Maps/AuthMap.js
@@ -12,9 +12,9 @@ export const AuthMap = () => {
       { latling: [{latitude: 34.18678806587779 , longitude: -117.32855070963915 }, {latitude: 34.1870591490249  , longitude: -117.328753669741 }]},
       { latling: [{latitude: 34.1864002295336 , longitude: -117.329575270922 }, {latitude: 34.1865744218195  , longitude: -117.329689444384 }]},
       { latling: [{latitude: 34.1865597225508 , longitude: -117.329118577078 }, {latitude:34.1861605657776  , longitude:-117.32885755788 }]},
-      { latling: [{latitude: 34.18617780288656 , longitude: -117.33010375379115 }, {latitude: 34.1824202456658 , longitude: -117.329512762842 }]}
-      //{ latling: [{latitude: , longitude: }, {latitude:  , longitude: }]},
-      //{ latling: [{latitude: , longitude: }, {latitude:  , longitude: }]},
+      { latling: [{latitude: 34.18617780288656 , longitude: -117.33010375379115 }, {latitude: 34.1824202456658 , longitude: -117.329512762842 }]},
+      { latling: [{latitude: 34.187025082191 , longitude: -117.328006120001 }, {latitude: 34.1855037971463 , longitude: -117.326365390507 }]},
+      { latling: [{latitude: 34.1862948987465 , longitude: -117.327189138634 }, {latitude: 34.1875404913909  , longitude: -117.325602592277 }]},
       //{ latling: [{latitude: , longitude: }, {latitude:  , longitude: }]},
       //{ latling: [{latitude: , longitude: }, {latitude:  , longitude: }]},
       //{ latling: [{latitude: , longitude: }, {latitude:  , longitude: }]},
