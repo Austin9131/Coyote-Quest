4a9d60ebe8544390441e39860dbf770e84159ff3
kadockham <kdockham2018@gmail.com>
2021-11-28 23:01:36 -0800

    polyline working

--- screens/components/Maps/AuthMap.js
+++ screens/components/Maps/AuthMap.js
@@ -1,8 +1,17 @@
 import MapView from 'react-native-maps';
+import  Polyline  from 'react-native-maps';
 import * as React from 'react';
+import { useState } from 'react';
 import { maps } from '../../../styles';
 
 export const AuthMap = () => {
+  state = {
+    polylines: [
+      { latling: [{latitude: 34.18725753, longitude: -117.3279266}, {latitude: 34.18701043 , longitude: -117.3279365}] },
+      { latling: [{latitude: 34.18701043, longitude: -117.3279365}, {latitude: 34.186198 , longitude: -117.3300622}]}
+
+    ]
+  }
     return(
         <MapView 
             ref={(ref) => this.mapRef = ref }
@@ -19,6 +28,16 @@ export const AuthMap = () => {
               { latitude: 34.188731, longitude: -117.330944 },
               { latitude: 34.176092, longitude: -117.313079 })}}
             minZoomLevel = {14.5}
-          />
+        >
+          {this.state.polylines.map( polyline => (
+            <MapView.Polyline
+              coordinates = {polyline.latling}
+              strokeColor="#000"
+              strokeWidth={4}
+              lineDashPattern={[1]}
+              lineCap = 'square'
+            />
+          ))}
+        </MapView>
     );
 };
