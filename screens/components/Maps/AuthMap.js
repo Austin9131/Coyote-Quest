47f938c547197c1047497f19123fab4599258517
Kenny Dockham <kdockham2018@gmail.com>
2021-11-29 10:08:33 -0800

    Changes to some of the lines and hopefully fixed the android issue with moving components

--- screens/components/Maps/AuthMap.js
+++ screens/components/Maps/AuthMap.js
@@ -11,13 +11,13 @@ export const AuthMap = () => {
   state = {
     polylines: [
       { latling: [{latitude: 34.18725753, longitude: -117.3279266}, {latitude: 34.18701043 , longitude: -117.3279365}] },
-      { latling: [{latitude: 34.18701043, longitude: -117.3279365}, {latitude: 34.186198 , longitude: -117.3300622}]},
+      { latling: [{latitude: 34.18701043, longitude: -117.3279365}, {latitude: 34.18678806587779 , longitude: -117.32855070963915 }, {latitude: 34.1865597225508 , longitude: -117.329118577078 }, {latitude: 34.1864002295336 , longitude: -117.329575270922 }, {latitude: 34.186198 , longitude: -117.3300622}]},
       { latling: [{latitude: 34.18678806587779 , longitude: -117.32855070963915 }, {latitude: 34.1870591490249  , longitude: -117.328753669741 }]},
       { latling: [{latitude: 34.1864002295336 , longitude: -117.329575270922 }, {latitude: 34.1865744218195  , longitude: -117.329689444384 }]},
       { latling: [{latitude: 34.1865597225508 , longitude: -117.329118577078 }, {latitude:34.1861605657776  , longitude:-117.32885755788 }]},
-      { latling: [{latitude: 34.18617780288656 , longitude: -117.33010375379115 }, {latitude: 34.1824202456658 , longitude: -117.329512762842 }]},
-      { latling: [{latitude: 34.187025082191 , longitude: -117.328006120001 }, {latitude: 34.1855037971463 , longitude: -117.326365390507 }]},
-      { latling: [{latitude: 34.1862948987465 , longitude: -117.327189138634 }, {latitude: 34.1875404913909  , longitude: -117.325602592277 }]},
+      { latling: [{latitude: 34.186198 , longitude: -117.3300622}, {latitude: 34.1824202456658 , longitude: -117.329512762842 }]},
+      { latling: [{latitude: 34.18701043 , longitude: -117.3279365 }, {latitude: 34.1862948987465 , longitude: -117.327189138634 }, {latitude: 34.1855037971463 , longitude: -117.326365390507 }]},
+      { latling: [{latitude: 34.1862948987465 , longitude: -117.327189138634 }, {latitude: 34.186919674139 , longitude: -117.326409187175 }, {latitude: 34.1875404913909  , longitude: -117.325602592277 }]},
       { latling: [{latitude: 34.186919674139 , longitude: -117.326409187175 }, {latitude: 34.1866204213817  , longitude: -117.326000607423 }]},
       //{ latling: [{latitude: , longitude: }, {latitude:  , longitude: }]},
       //{ latling: [{latitude: , longitude: }, {latitude:  , longitude: }]},
